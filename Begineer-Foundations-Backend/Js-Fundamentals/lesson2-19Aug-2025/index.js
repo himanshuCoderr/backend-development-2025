083417ee74b7a8d44181b828b173b748cda90c65
himanshuCoderr <malhotrahimanshu002@gmail.com>
2025-09-08 10:28:28 +0530

    lesson2-js variables-js-foundation

---
+++ Begineer-Foundations-Backend/Js-Fundamentals/lesson2-19Aug-2025/index.js
@@ -0,0 +1,101 @@
+// variable 
+
+//  programme me agr hume kisi bhi data ko store karna h then we need a variable to store that data we need a variable 
+
+//  variable is like a container
+
+// console.log("Hey")
+
+// key word -> reserved words that is used for special work
+
+// let name = "Himanshu"
+// name = "Happy"
+
+
+// name variable is assigned with "Himanshu" string data
+
+
+// console.log(name)
+
+
+
+
+// Three keywords , let , var , const
+
+// let var const 
+
+// Assignment 
+
+// re assignment
+
+
+// declaration 
+
+// re declaration
+
+
+// declaration 
+
+// let age = 12
+
+// redeclaration
+ 
+// let age
+
+//  re assignment
+// age = 14
+
+// console.log(age)
+
+
+// let var const 
+
+// let >  redeclartion  -> not allowed , reassignment is allowed 
+
+// var -> redeclaration is allowed , reassignment is also allowed 
+
+// const -> redeclaration is not allowed , re assignment is not allowed 
+
+
+// let 
+
+
+// let name = "Himanshu"
+
+// let name = "Happy"
+// name  = "Samfcdsfds"
+// console.log(name)
+
+// var 
+
+// var name = "Himanshu"
+// console.log(name)
+
+// var name = "Happy"
+
+// console.log(name)
+
+
+// const name = "Himanshu"
+
+// name = "Happy"
+
+// console.log(name)
+
+
+// block code 
+
+// let name ="Himanshu"
+
+// console.log(name)
+
+// {
+    
+//     let name = "Happy"
+
+//     let name ="Hi"
+//     console.log(name)
+
+// }
+
+// Same block of code 
